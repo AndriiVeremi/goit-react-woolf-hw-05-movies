e5460a5478349e327a8a337adfdafdc3adcac0a9
Andrii Veremii <dashuk10@gmail.com>
2024-03-03 16:32:45 +0100

    add

--- src/components/MoviesSearchForm/MoviesSearchForm.styled.js
+++ src/components/MoviesSearchForm/MoviesSearchForm.styled.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const SearchFormWrapper = styled.div`
 height: 80px;
-/* padding: 10px; */
 align-items: center;
 display: flex;
 justify-content: center;
@@ -22,10 +21,23 @@ width: 360px;
 height: 40px;
 font-size: 24px;
 border: 1px solid #333;
-
+border-radius: 8px;
+padding: 0px 10px;
 ` 
 
 export const SearchFormBtn = styled.button`
-font-size: 20px;
-padding: 8px 16px;
+font-size: 18px;
+ padding: 8px 22px;
+  background-color: #fff;
+  border-radius: 10px;
+  border: 1px solid #5d5d5d;
+  color: #5d5d5d;
+  margin-left: auto;
+  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
+ 
+  &:hover {
+    background-color: #4f4f4f;
+    border: 1px solid #4f4f4f;
+    color: #fff;
+  }
 ` 
